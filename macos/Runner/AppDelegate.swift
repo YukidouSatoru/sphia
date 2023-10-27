6a615652cec8b79eb9a1f1f0e42685484a51720e
YukidouSatoru <yukidousatoru@gmail.com>
2023-10-28 02:19:05 +0800

    Implement applicationSupportsSecureRestorableState

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -18,4 +18,7 @@ class AppDelegate: FlutterAppDelegate {
     }
     return true
   }
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
 }
